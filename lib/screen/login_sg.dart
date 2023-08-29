987bba8708eda8ed95c91d3c6affa330dbde0300
koushiksg <kousik.sengupta@gmail.com>
2023-08-29 16:47:07 +0530

    UserAPI_29082023

--- lib/screen/login_sg.dart
+++ lib/screen/login_sg.dart
@@ -17,6 +17,7 @@ class _LoginAppState extends State<LoginApp> {
   var dropdownvalue;
   var _errorMessage;
   List<dynamic> categoryItemlist = [];
+  List<dynamic> userlist = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,9 +59,13 @@ class _LoginAppState extends State<LoginApp> {
             const SizedBox(height: 16.0),
             ElevatedButton(
               onPressed: () {
+                //getUser();
                 String username = _usernameController.text;
                 String password = _passwordController.text;
-                 print(dropdownvalue);
+                 //print(dropdownvalue);
+                //getUser();
+            //final int IdUsr = Userlist[0]['IdUsr'];
+            print (userlist.length);
                 if (username == 'admin' && password == 'admin') {
                   Navigator.push(
                     context,
@@ -71,7 +76,6 @@ class _LoginAppState extends State<LoginApp> {
                   showAlertDialog(context);
                   setState(() {
                     _errorMessage = 'Invalid username or password';
-                    
                       
                   });
                 }
@@ -105,7 +109,7 @@ class _LoginAppState extends State<LoginApp> {
 
   Future getAllCategory() async {
     //const url = 'http://rcssoft.in/DocMgmt/api/Location/?LocId=0&LocName=';
-    const url = 'http://rcssoft.in/DocMgmt/api/Location/?LocId=0&LocName=';
+    const url = 'http://rcssoft.in/DocMgmt/api/Location/?LocId=0&LocName=';    
     final uri = Uri.parse(url);
     final response = await http.get(uri);
     final body = response.body;
@@ -114,9 +118,33 @@ class _LoginAppState extends State<LoginApp> {
     });
   }
 
+  Future getUser() async {
+    //const url = 'http://rcssoft.in/DocMgmt/api/Location/?LocId=0&LocName=';
+    int LocId;
+    if (dropdownvalue != null)
+    {
+LocId = dropdownvalue;
+    }
+     else
+     {
+      LocId = 0;
+     }
+    final url = 'http://rcssoft.in/DocMgmt/api/User/?IdUsr=0&UserName=&LocId=$LocId';
+    //const url = 'http://rcssoft.in/DocMgmt/api/User/?IdUsr=0&UserName=&LocId=0';
+    print(url);
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    final body = response.body;
+    setState(() {
+     userlist = jsonDecode(body);
+    });
+    //print (userlist.length);
+  }
+
   @override
   void initState() {
     super.initState();
     getAllCategory();
+    getUser();
   }
 }
