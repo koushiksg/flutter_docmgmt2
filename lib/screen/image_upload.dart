c254d60b1cf6cec5db6e18d701a388ce7bec077f
koushiksg <kousik.sengupta@gmail.com>
2024-02-17 18:07:46 +0530

    DocMgmt2_17022024

--- lib/screen/image_upload.dart
+++ lib/screen/image_upload.dart
@@ -6,6 +6,8 @@ import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:image_picker/image_picker.dart';
+
 class ImageUploadScreen extends StatefulWidget {
   @override
   State<ImageUploadScreen> createState() => _ImageUploadScreenState();
@@ -14,8 +16,28 @@ class ImageUploadScreen extends StatefulWidget {
 class _ImageUploadScreenState extends State<ImageUploadScreen> {
   List<int>? _selectedFile;
   Uint8List? _bytesData;
-//  String selectfile;
+  //Uint8List _bytesData;
+  //List<Uint8List>? _bytesData;
+//  String selectfile = '';
+//  XFile file;
 //  Uint8List selectedImageInBytes;
+  List<Uint8List> pickedImagesInBytes = [];
+//  int imageCounts = 0;
+
+//  _selectFile(bool imageFrom) async {
+//    FilePickerResult fileResult =
+//        await FilePicker.platform.pickFiles(allowMultiple: true);
+//    if (fileResult != null) {
+//      selectfile = fileResult.files.first.name;
+//      fileResult.files.forEach((element) {
+//        setState(() {
+//          pickedImagesInBytes.add(element.bytes);
+          //selectedImageInBytes = fileResult.files.first.bytes;
+//        });
+//      });
+//    }
+//    print(selectfile);
+//  }
 
   startWebFilePicker() async {
     html.FileUploadInputElement uploadInput = html.FileUploadInputElement();
@@ -34,22 +56,21 @@ class _ImageUploadScreenState extends State<ImageUploadScreen> {
         });
       });
       reader.readAsDataUrl(file);
+      //    if (fileResult != null) {
+//      selectfile = fileResult.files.first.name;
+//      fileResult.files.forEach((element) {
+//        setState(() {
+//          pickedImagesInBytes.add(element.bytes);
+          //selectedImageInBytes = fileResult.files.first.bytes;
+//        });
       final mlen = files.length;
       for (var i = 0; i < mlen; i++) {
         print(files[i].name);
+        print(_selectedFile);
       }
     });
   }
 
-//_selectFile(bool imageFrom) async {
-//  FilePickerResult fileResult = await FilePicker.platform.pickFiles();
-//  if (fileResult != null) {
-//    setState(() {
-//      selectfile = fileResult.files.first.name;
-//
-//    });
-//  }
-//}
   Future uploadImage() async {
     var url = Uri.parse("uri");
   }
@@ -83,39 +104,40 @@ class _ImageUploadScreenState extends State<ImageUploadScreen> {
               //_bytesData != null
               //    ? Image.memory(_bytesData!, width: 200, height: 200)
               //    :
-                  //: Container(),
-                 //_bytesData != null
-                  //?
-                  _bytesData != null
+              //: Container(),
+              //_bytesData != null
+              //?
+              _bytesData != null
                   ? CarouselSlider(
-  options: CarouselOptions(height: 400.0),
-  items:_selectedFile!.map((i) {
-    return Builder(
-      builder: (BuildContext context) {
-        return Container(
-          width: MediaQuery.of(context).size.width,
-          margin: EdgeInsets.symmetric(horizontal: 5.0),
-          decoration: BoxDecoration(
-            color: Colors.amber
-          ),
-//child: Image.memory(_bytesData!, width: 200, height: 200)
-child: Text('text $i', style: TextStyle(fontSize: 16.0),)
-        );
-      },
-    );
-  }).toList(),
-):
-              MaterialButton(
-                color: Colors.purple,
-                elevation: 8,
-                highlightElevation: 2,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                textColor: Colors.white,
-                child: Text('Send File to Server'),
-                onPressed: () {},
-              )
+                      options: CarouselOptions(height: 400.0),
+                      items: _bytesData!.map((i) {
+                        return Builder(
+                          builder: (BuildContext context) {
+                            return Container(
+                                width: MediaQuery.of(context).size.width,
+                                margin: EdgeInsets.symmetric(horizontal: 5.0),
+                                decoration: BoxDecoration(color: Colors.amber),
+child: Image.memory(_bytesData!)
+                                //child: Text(
+                                //  'text $i',
+                                //  style: TextStyle(fontSize: 16.0),
+                                //)
+                                );
+                          },
+                        );
+                      }).toList(),
+                    )
+                  : MaterialButton(
+                      color: Colors.purple,
+                      elevation: 8,
+                      highlightElevation: 2,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      textColor: Colors.white,
+                      child: Text('Send File to Server'),
+                      onPressed: () {},
+                    )
             ],
           ),
         ),
